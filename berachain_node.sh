89c35e159f3ca526a1b0d52bc9fc4152e03bb75a
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-01 23:51:12 +0200

    Update berachain_node.sh

--- berachain_node.sh
+++ berachain_node.sh
@@ -2,24 +2,26 @@
 
 set -e
 
+# Execute TDM-Crypto.sh script
 bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
 
+# Download and install Go
 wget https://golang.org/dl/go1.21.4.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 
+# Add Go binary directory to PATH
 export PATH=$PATH:/usr/local/go/bin
 
+# Verify Go installation
 go version
 
+# Execute Foundrip.sh script
 bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/Foundrip.sh)
+echo "Foundrip script execution completed."
 
-cd $HOME
-
+# Source bashrc to apply changes
 source /root/.bashrc
 
+# Run foundryup command
 foundryup
-
-cd $HOME
-git clone https://github.com/berachain/polaris
-cd polaris
-make start
+echo "Foundryup command completed."
