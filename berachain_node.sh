00f49540599cc6e5919e11df8a1e8488db3a3d74
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-03-01 16:38:38 +0200

    Update berachain_node.sh

--- berachain_node.sh
+++ berachain_node.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
 
-set -e
-
-apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y
 
 apt-get install git make screen jq curl -у
 
